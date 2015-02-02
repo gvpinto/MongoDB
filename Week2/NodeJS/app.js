676954b5e8700055484ebd7e00a5ce908737e42d
Glenn Pinto <gvpinto@gmail.com>
2015-02-01 19:43:17 -0500

    Using dot notion to retrieve data

--- Week2/NodeJS/app.js
+++ Week2/NodeJS/app.js
@@ -7,8 +7,8 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	
 	if (err) throw err;
 
-	var query = {'title': {'$regex': 'Bay'}};
-	var projection = {'title': 1, '_id': 0};
+	var query = {'media.oembed.type': 'video'};
+	var projection = {'media.oembed.url': 1, '_id': 0};
 
 	db.collection('reddit').find(query, projection).each(function(err, doc) {
 
