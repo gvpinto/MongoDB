5e5abcc4ac7571d8875654af286e0130e6b34242
Glenn Pinto <gvpinto@gmail.com>
2015-02-01 23:12:48 -0500

    Inserting records. Eg. 4. Inserting multiple documents

--- Week2/NodeJS/app.js
+++ Week2/NodeJS/app.js
@@ -1,4 +1,4 @@
-// Inserting records. Eg. 3. Changing the code to handle the error
+// Inserting records. Eg. 4. Inserting multiple documents
 
 var MongoClient = require('mongodb').MongoClient;
 
@@ -6,9 +6,10 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	
 	if (err) throw err;
 	
-	var student = {'_id': 'Calvin', 'age': 6};
+	var students = [{'student': 'Calvin', 'age': 6}, 
+					{'student': 'Amy', 'age': 7}];
 
-	db.collection('students').insert(student, function(err, inserted) {
+	db.collection('students').insert(students, function(err, inserted) {
 
 		if (err) {
 			console.log(err.message);
