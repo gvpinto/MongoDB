ede7fef0a515343c4effb61246e93e544ef26631
Glenn Pinto <gvpinto@gmail.com>
2015-02-01 21:33:29 -0500

    Skip limit and Sort. Eg. 1

--- Week2/NodeJS/app.js
+++ Week2/NodeJS/app.js
@@ -1,4 +1,4 @@
-// Dot notion for Querying
+// Skip limit and Sort. Eg. 1
 
 var MongoClient = require('mongodb').MongoClient,
 request = require('request');
@@ -7,10 +7,13 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	
 	if (err) throw err;
 
-	var query = {'media.oembed.type': 'video'};
-	var projection = {'media.oembed.url': 1, '_id': 0};
+	var grades = db.collection('grades');
+	var cursor = grades.find({});
+	cursor.skip(1);
+	cursor.limit(4);
+	cursor.sort({'grade': 1});
 
-	db.collection('reddit').find(query, projection).each(function(err, doc) {
+	cursor.each(function(err, doc) {
 
 		if (err) throw err;
 		
@@ -19,7 +22,6 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 		}
 		
 		console.dir(doc);
-
 
 	});
 	
