09e1e6d5af37942c04eb3405749c6b96f748f93f
Glenn Pinto <gvpinto@gmail.com>
2015-02-01 22:59:06 -0500

    Inserting records

--- Week2/NodeJS/app.js
+++ Week2/NodeJS/app.js
@@ -1,41 +1,21 @@
-// Skip limit and Sort. Eg. 4
+// Inserting records
 
-var MongoClient = require('mongodb').MongoClient,
-request = require('request');
+var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	
 	if (err) throw err;
+	
+	var student = {'name': 'Calvin', 'age': 6};
 
-	var grades = db.collection('grades');
-	var cursor = grades.find({});
-	//cursor.sort({'grade': 1});
-	//cursor.sort('grade', 1);
-	cursor.limit(4);
-	cursor.skip(1);
-	
-	// Sort Order in an Array does not change when it gets to the database
-	//cursor.sort([['grade', 1], ['student', -1]]);
-	
-	// Do not use Object as the sort attribute order can change
-	//cursor.sort({'grade': 1, 'student': -1});
-	var options = {
-		'limit': 4,
-		'skip': 1,
-		'sort': [['grade', 1], ['student', -1]]};
-		
-	var cursor = grades.find({}, {}, options);
-	
-	cursor.each(function(err, doc) {
+	db.collection('students').insert(student, function(err, inserted) {
 
 		if (err) throw err;
 		
-		if (doc == null) {
-			return db.close()
-		}
+		console.log('Record has been sucessfully inserted: ' + JSON.stringify(inserted));
+		return db.close();
 		
-		console.dir(doc);
-
 	});
+
 	
 });
