304f1dd8a3c4b2b53c45e8da5fa3c701fae71826
Glenn Pinto <gvpinto@gmail.com>
2015-02-01 23:04:36 -0500

    Inserting records. Eg. 2. Providing an _id value and trying to insert it twice

--- Week2/NodeJS/app.js
+++ Week2/NodeJS/app.js
@@ -1,4 +1,4 @@
-// Inserting records
+// Inserting records. Eg. 2. Providing an _id value and trying to insert it twice
 
 var MongoClient = require('mongodb').MongoClient;
 
@@ -6,7 +6,7 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	
 	if (err) throw err;
 	
-	var student = {'name': 'Calvin', 'age': 6};
+	var student = {'_id': 'Calvin', 'age': 6};
 
 	db.collection('students').insert(student, function(err, inserted) {
 
