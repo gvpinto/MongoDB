0dbf78827735becd1631a34a61585c362e48c833
Glenn Pinto <gvpinto@gmail.com>
2015-02-01 23:16:09 -0500

    Inserting records. Eg. 5. Inserting multiple documents with _id but varying case

--- Week2/NodeJS/app.js
+++ Week2/NodeJS/app.js
@@ -1,4 +1,4 @@
-// Inserting records. Eg. 4. Inserting multiple documents
+// Inserting records. Eg. 5. Inserting multiple documents with _id but varying case
 
 var MongoClient = require('mongodb').MongoClient;
 
@@ -6,8 +6,8 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
 	
 	if (err) throw err;
 	
-	var students = [{'student': 'Calvin', 'age': 6}, 
-					{'student': 'Amy', 'age': 7}];
+	var students = [{'_id': 'Renee', 'age': 13}, 
+					{'_id': 'renee', 'age': 14}];
 
 	db.collection('students').insert(students, function(err, inserted) {
 
